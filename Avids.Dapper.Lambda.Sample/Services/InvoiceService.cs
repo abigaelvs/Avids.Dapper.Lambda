e46825021c6b760ce47ca69b047a11ffdd93f524
abigaelvs <abigaelvas@gmail.com>
2023-12-19 15:39:29 +0700

    docs: adjust docs have group by

--- Avids.Dapper.Lambda.Sample/Services/InvoiceService.cs
+++ Avids.Dapper.Lambda.Sample/Services/InvoiceService.cs
@@ -23,12 +23,12 @@ namespace Avids.Dapper.Lambda.Sample.Services
                 .LeftJoin((Cashier cashier, SearchInvoiceList inv) => cashier.Id == inv.CashierId)
                 .RightJoin((Customer cust, SearchInvoiceList inv) => cust.Id == inv.CustomerId)
                 .FullJoin((PaymentStatus stat, SearchInvoiceList inv) => stat.Id == inv.PaymentStatusId)
-                .GroupBy(inv => inv.No).OrderBy(inv => inv.Id)
-                .Select(inv => inv)
-                .Select(inv => new Cashier { Id = inv.CashierId, Name = inv.CreatedBy })
-                .Select(inv => new Customer { Id = inv.CustomerId, Name = inv.CustomerName })
-                .Select(inv => new InvoiceStatus { Id = inv.StatusId, Name = inv.StatusName })
-                .Select(inv => new PaymentStatus{ Id = inv.PaymentStatusId, Name = inv.PaymentStatusName })
+                .GroupBy(inv => inv.Id).OrderBy(inv => inv.Id)
+                .Select(inv => inv.Id)
+                //.Select(inv => new Cashier { Id = inv.CashierId, Name = inv.CreatedBy })
+                //.Select(inv => new Customer { Id = inv.CustomerId, Name = inv.CustomerName })
+                //.Select(inv => new InvoiceStatus { Id = inv.StatusId, Name = inv.StatusName })
+                //.Select(inv => new PaymentStatus{ Id = inv.PaymentStatusId, Name = inv.PaymentStatusName })
                 .Distinct().Limit(1).Offset(10);
 
             IEnumerable<SearchInvoiceList> result = await invoices.ToListAsync();
