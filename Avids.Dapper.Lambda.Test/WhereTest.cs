a5279c706655c49b1db701f2f7dbdff791824320
abigaelvs <abigaelvas@gmail.com>
2024-01-02 22:30:58 +0700

    test: add where test

---
+++ Avids.Dapper.Lambda.Test/WhereTest.cs
@@ -0,0 +1,30 @@
+﻿using Avids.Dapper.Lambda.Test.Entity;
+using Npgsql;
+
+namespace Avids.Dapper.Lambda.Test
+{
+    public class WhereTest
+    {
+        [Fact]
+        public void TestWhere()
+        {
+            List<long> ids = new() { 1, 2, 3 };
+            List<string> nos = new() { "IV1", "IV2" };
+            string expected = @"SELECT  * FROM ""Invoice""   WHERE ""Id"" = @Id1 AND ""StatusId"" > @StatusId2 "
+                + @"AND ""StatusId"" < @StatusId3 OR ""StatusId"" <= @StatusId4 AND ""StatusId"" >= @StatusId5 "
+                + @"AND ""Id"" != @Id6 OR ""No"" LIKE @No7 AND ""No"" LIKE @No8 AND ""No"" LIKE @No9 "
+                + @"OR ""No"" NOT LIKE @No10 AND ""No"" NOT LIKE @No11 AND ""No"" NOT LIKE @No12 "
+                + @"OR ""Id"" IN @Id13 AND ""No"" IN @No14 AND ""No"" IS NULL";
+            SqlProvider provider = new NpgsqlConnection().QuerySet<Invoice>()
+                .Where(inv => inv.Id == 1 && inv.StatusId > 1 && inv.StatusId < 1 
+                || inv.StatusId <= 1 && inv.StatusId >= 1 && inv.Id != 1 
+                || inv.No.Contains("IV") && inv.No.StartsWith("IV") && inv.No.EndsWith("IV") 
+                || !inv.No.Contains("IV") && !inv.No.StartsWith("IV") && !inv.No.EndsWith("IV")
+                || ids.Contains(inv.Id) && nos.Contains(inv.No) && inv.No == null)
+                .SqlProvider;
+
+            string actual = provider.FormatToList<Invoice>().SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
