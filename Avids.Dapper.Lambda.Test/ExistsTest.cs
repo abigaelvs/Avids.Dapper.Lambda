7f113f9da7b0a0ada3490e3ba94f13b7c77156f9
abigaelvs <abigaelvas@gmail.com>
2024-01-21 17:14:54 +0700

    test: add exists test

---
+++ Avids.Dapper.Lambda.Test/ExistsTest.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+
+using Avids.Dapper.Lambda.Test.Entity;
+
+namespace Avids.Dapper.Lambda.Test
+{
+    public class ExistsTest
+    {
+        [Fact]
+        public void TestExists()
+        {
+            string expected = @"SELECT 1 FROM ""Invoice""  WHERE ""Id"" = @Id1 LIMIT 1";
+            string actual = new NpgsqlConnection().CommandSet<Invoice>()
+                .Where(inv => inv.Id == 1).SqlProvider
+                .FormatExists().SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
