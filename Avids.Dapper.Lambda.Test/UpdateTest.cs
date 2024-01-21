d2258b9a568afa4c55624a08cee0769931325565
abigaelvs <abigaelvas@gmail.com>
2024-01-21 17:15:15 +0700

    test: add test update select

--- Avids.Dapper.Lambda.Test/UpdateTest.cs
+++ Avids.Dapper.Lambda.Test/UpdateTest.cs
@@ -8,14 +8,13 @@ namespace Avids.Dapper.Lambda.Test
         [Fact]
         public void TestUpdate()
         {
-            string expected = @"UPDATE ""Invoice""  SET  ""Id""=@UPDATE_Id , ""No""=@UPDATE_No , "
+            string expected = @"UPDATE ""Invoice""  SET  ""No""=@UPDATE_No , "
                 + @"""StatusId""=@UPDATE_StatusId , ""PaymentStatusId""=@UPDATE_PaymentStatusId , "
                 + @"""CashierId""=@UPDATE_CashierId , ""UpdatedByUserId""=@UPDATE_UpdatedByUserId , "
                 + @"""CustomerId""=@UPDATE_CustomerId , ""CreatedDate""=@UPDATE_CreatedDate , "
                 + @"""UpdatedDate""=@UPDATE_UpdatedDate";
 
             Invoice inv = new();
-            inv.Id = 1;
             inv.No = "IV123";
             inv.CustomerId = 1;
             inv.CashierId = 1;
@@ -32,14 +31,13 @@ namespace Avids.Dapper.Lambda.Test
         [Fact]
         public void TestUpdateWithWhere()
         {
-            string expected = @"UPDATE ""Invoice""  SET  ""Id""=@UPDATE_Id , ""No""=@UPDATE_No , "
+            string expected = @"UPDATE ""Invoice""  SET  ""No""=@UPDATE_No , "
                     + @"""StatusId""=@UPDATE_StatusId , ""PaymentStatusId""=@UPDATE_PaymentStatusId , "
                     + @"""CashierId""=@UPDATE_CashierId , ""UpdatedByUserId""=@UPDATE_UpdatedByUserId , "
                     + @"""CustomerId""=@UPDATE_CustomerId , ""CreatedDate""=@UPDATE_CreatedDate , "
                     + @"""UpdatedDate""=@UPDATE_UpdatedDate   WHERE ""Id"" = @Id1";
 
             Invoice inv = new();
-            inv.Id = 1;
             inv.No = "IV123";
             inv.CustomerId = 1;
             inv.CashierId = 1;
@@ -51,6 +49,32 @@ namespace Avids.Dapper.Lambda.Test
             string actual = new NpgsqlConnection().CommandSet<Invoice>()
                 .Where(inv => inv.Id == 1)
                 .SqlProvider.FormatUpdate(inv).SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestUpdateSelect()
+        {
+            string expected = @"UPDATE ""Invoice""  SET  ""No""=@UPDATE_No , "
+                    + @"""StatusId""=@UPDATE_StatusId , ""PaymentStatusId""=@UPDATE_PaymentStatusId , "
+                    + @"""CashierId""=@UPDATE_CashierId , ""UpdatedByUserId""=@UPDATE_UpdatedByUserId , "
+                    + @"""CustomerId""=@UPDATE_CustomerId , ""CreatedDate""=@UPDATE_CreatedDate , "
+                    + @"""UpdatedDate""=@UPDATE_UpdatedDate   WHERE ""Id"" IN "
+                    + @"(SELECT ""Id"" FROM ""Invoice"" WHERE ""Id"" = @Id1  FOR UPDATE SKIP LOCKED) "
+                    + @"RETURNING *";
+
+            Invoice inv = new();
+            inv.No = "IV123";
+            inv.CustomerId = 1;
+            inv.CashierId = 1;
+            inv.StatusId = 1;
+            inv.CreatedDate = DateTime.Now;
+            inv.UpdatedByUserId = null;
+            inv.UpdatedDate = null;
+
+            string actual = new NpgsqlConnection().CommandSet<Invoice>()
+                .Where(inv => inv.Id == 1)
+                .SqlProvider.FormatUpdateSelect(inv).SqlString.Trim();
             Assert.Equal(expected, actual);
         }
     }
