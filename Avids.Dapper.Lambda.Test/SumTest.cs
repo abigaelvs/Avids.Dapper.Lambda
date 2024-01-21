901bffa3fb2d914190fd9a688dd2423f918089f8
abigaelvs <abigaelvas@gmail.com>
2024-01-21 17:39:29 +0700

    test: add test sum ms sql and test sumc mysql

--- Avids.Dapper.Lambda.Test/SumTest.cs
+++ Avids.Dapper.Lambda.Test/SumTest.cs
@@ -1,4 +1,6 @@
 ﻿using Npgsql;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 using Avids.Dapper.Lambda.Test.Entity;
 
@@ -11,6 +13,24 @@ namespace Avids.Dapper.Lambda.Test
         {
             string expected = @"SELECT COALESCE(SUM(""Id""),0)   FROM ""Invoice""";
             string actual = new NpgsqlConnection().CommandSet<Invoice>()
+                .SqlProvider.FormatSum<Invoice, long>(inv => inv.Id).SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestSumMsSql()
+        {
+            string expected = @"SELECT ISNULL(SUM([Id]),0)   FROM [Invoice]";
+            string actual = new SqlConnection().CommandSet<Invoice>()
+                .SqlProvider.FormatSum<Invoice, long>(inv => inv.Id).SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestSumMySql()
+        {
+            string expected = @"SELECT IFNULL(SUM(`Id`),0)   FROM `Invoice`";
+            string actual = new MySqlConnection().CommandSet<Invoice>()
                 .SqlProvider.FormatSum<Invoice, long>(inv => inv.Id).SqlString.Trim();
             Assert.Equal(expected, actual);
         }
