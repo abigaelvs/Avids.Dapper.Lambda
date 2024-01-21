f4ae24922893b3a22d8c3fb4df7e39be50e806e0
abigaelvs <abigaelvas@gmail.com>
2024-01-21 15:26:38 +0700

    test: add delete test

---
+++ Avids.Dapper.Lambda.Test/DeleteTest.cs
@@ -0,0 +1,27 @@
+﻿using Avids.Dapper.Lambda.Test.Entity;
+using Npgsql;
+
+namespace Avids.Dapper.Lambda.Test
+{
+    public class DeleteTest
+    {
+        [Fact]
+        public void TestDelete()
+        {
+            string expected = @"DELETE FROM ""Invoice""";
+            string actual = new NpgsqlConnection().CommandSet<Invoice>()
+                .SqlProvider.FormatDelete().SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDeleteWithWhere()
+        {
+            string expected = @"DELETE FROM ""Invoice"" WHERE ""Id"" = @Id1";
+            string actual = new NpgsqlConnection().CommandSet<Invoice>()
+                .Where(inv => inv.Id == 1)
+                .SqlProvider.FormatDelete().SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
