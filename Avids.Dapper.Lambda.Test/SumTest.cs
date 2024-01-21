46b9671e4f26b144e9f41fd04c45ba8fe27b8b4c
abigaelvs <abigaelvas@gmail.com>
2024-01-21 17:15:00 +0700

    test: add sum test

---
+++ Avids.Dapper.Lambda.Test/SumTest.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+
+using Avids.Dapper.Lambda.Test.Entity;
+
+namespace Avids.Dapper.Lambda.Test
+{
+    public class SumTest
+    {
+        [Fact]
+        public void TestSum()
+        {
+            string expected = @"SELECT COALESCE(SUM(""Id""),0)   FROM ""Invoice""";
+            string actual = new NpgsqlConnection().CommandSet<Invoice>()
+                .SqlProvider.FormatSum<Invoice, long>(inv => inv.Id).SqlString.Trim();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
